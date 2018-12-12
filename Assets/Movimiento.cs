f29313b64fabb566b0cb3845176702d866b5a998
Gaizka Carrancedo Gilsanz <klazzus@gmail.com>
2018-12-12 20:53:23 +0100

    181212

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+    float Izq = 1;
+    float Der = -1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Caida = new Vector3(0, -0.01f, 0);
+        transform.Translate(Caida);
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(new Vector3(Izq, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(new Vector3(Der, 0, 0));
+        }
+
+
+    }
+}
